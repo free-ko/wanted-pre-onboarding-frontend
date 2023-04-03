63ed152545fcdab4668714f115eeb1e7e6d074dc
Kay <duddnr654@naver.com>
2023-04-03 16:05:47 +0900

    [FEAT]: 글로벌 CSS 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Global } from "@emotion/react";
+
+import { globalStyle } from "./styles";
 
 import App from "./App";
 
@@ -8,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
+      <Global styles={globalStyle} />
       <App />
     </Router>
   </React.StrictMode>
