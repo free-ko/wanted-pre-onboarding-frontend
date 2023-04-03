d1ab6e6e3c6b4561b0a951c767dffd5f07acbe69
Kay <duddnr654@naver.com>
2023-04-03 18:20:54 +0900

    [FEAT]: Main 페이지 UI 제거 후, default 페이지 Login 페이지로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 
-import { MainPage, LoginPage, SignupPage } from "./pages";
+import { LoginPage, SignupPage } from "./pages";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<LoginPage />} />
       <Route path="/signin" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
     </Routes>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,2 @@
-export { default as MainPage } from "./MainPage";
 export { default as LoginPage } from "./LoginPage";
 export { default as SignupPage } from "./SignupPage";
