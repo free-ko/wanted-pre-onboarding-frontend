afff2b23c3163191c9ee80a0c62610349aecc30c
Kay <duddnr654@naver.com>
2023-04-09 18:58:24 +0900

    [FIX]: 회원가입 페이지에서 토큰 있을 시, 자동으로 todo 페이지 이동

--- src/pages/Signup/SignupPage.tsx
+++ src/pages/Signup/SignupPage.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
 import { SignupForm } from "src/components";
 
 const SignupPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/todo");
+    }
+  }, [navigate]);
+
   return <SignupForm />;
 };
 
